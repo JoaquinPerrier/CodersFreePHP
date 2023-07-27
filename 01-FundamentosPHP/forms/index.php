a126a81fb1e557bbfde0eb57671531e34f93a86d
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-07-26 23:53:12 -0300

    Reacomodando carpetas

---
+++ 01-FundamentosPHP/forms/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- En action se pone a donde se envia la data -->
+    <form action="">
+
+    </form>
+</body>
+</html>
